472c5964a4dfc5ed927c8f02cb866e1a5e68b500
janggihun <jgh7111@gmail.com>
2024-03-29 01:00:30 +0900

    login - success

--- src/main/java/com/jdevil/cms/login/controller/LoginController.java
+++ src/main/java/com/jdevil/cms/login/controller/LoginController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +14,9 @@ import com.jdevil.cms.global.dto.MemberDto;
 import com.jdevil.cms.login.service.LoginService;
 
 import lombok.extern.slf4j.Slf4j;
+
+
+
 
 @Controller
 @Slf4j
@@ -32,11 +34,11 @@ public class LoginController {
 	@PostMapping("/login")
 	public String login(MemberDto memberDto, Model model, HttpSession session) {
 
-		System.out.println("id 확인중...");
 
 		HashMap<String, Object> rMap = loginService.idcheck(memberDto);
-		log.info("id확인중");
-		System.out.println("확인완료");
+		
+
+		log.info("aaa");
 		if (rMap != null) {
 
 			session.setAttribute("userId", memberDto.getUserId());
